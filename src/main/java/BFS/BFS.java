bdf20629e67d2917d4b6bc053cce8be5a4c944f4
nicholas-hgit <nicholas.hlungwane@gmail.com>
2024-08-17 12:09:43 +0200

    refactored BFS

--- src/main/java/BFS/BFS.java
+++ src/main/java/BFS/BFS.java
@@ -1,14 +1,12 @@
 package BFS;
 
-import Game.GameStage;
 import Node.Node;
+import static Direction.Direction.*;
 
 import javax.swing.*;
-import java.awt.*;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import static Direction.Direction.*;
-import static java.awt.Color.PINK;
+
 
 public class BFS {
 
@@ -31,54 +29,52 @@ public class BFS {
             if(currentNode.y() != 0){
 
                 Node leftNode = maze[currentNode.x()][currentNode.y() + LEFT.move()];
+                visitPotentialNode(canvas,currentNode,leftNode);
 
-                if(leftNode.isNotVisited() && leftNode.isNotObstacle()){
-
-                    leftNode.setVisited(true);
-                    leftNode.setParent(currentNode);
-                    nodes.offer(leftNode);
-                    canvas.repaint();
+                if(leftNode.isGoal()){
+                    break;
                 }
             }
 
             if(currentNode.x() != maze.length - 1){
 
                 Node bottomNode = maze[currentNode.x() + DOWN.move()][currentNode.y()];
+                visitPotentialNode(canvas,currentNode,bottomNode);
 
-                if(bottomNode.isNotVisited() && bottomNode.isNotObstacle()){
-
-                    bottomNode.setVisited(true);
-                    bottomNode.setParent(currentNode);
-                    nodes.offer(bottomNode);
-                    canvas.repaint();
+                if(bottomNode.isGoal()){
+                    break;
                 }
             }
 
             if(currentNode.y() != maze.length - 1){
 
                 Node rightNode = maze[currentNode.x()][currentNode.y() + RIGHT.move()];
+                visitPotentialNode(canvas,currentNode,rightNode);
 
-                if(rightNode.isNotVisited() && rightNode.isNotObstacle()){
-
-                    rightNode.setVisited(true);
-                    rightNode.setParent(currentNode);
-                    nodes.offer(rightNode);
-                    canvas.repaint();
+                if(rightNode.isGoal()){
+                    break;
                 }
             }
 
             if(currentNode.x() != 0){
 
                 Node topNode = maze[currentNode.x() + UP.move()][currentNode.y()];
+                visitPotentialNode(canvas,currentNode,topNode);
 
-                if (topNode.isNotVisited() && topNode.isNotObstacle()){
-
-                    topNode.setVisited(true);
-                    topNode.setParent(currentNode);
-                    nodes.offer(topNode);
-                    canvas.repaint();
+                if(topNode.isGoal()){
+                    break;
                 }
             }
+        }
+    }
+
+    private void visitPotentialNode(JPanel canvas, Node current, Node neighbour) {
+        if(neighbour.isNotVisited() && neighbour.isNotObstacle()){
+
+            neighbour.setVisited(true);
+            neighbour.setParent(current);
+            nodes.offer(neighbour);
+            canvas.repaint();
         }
     }
 }
